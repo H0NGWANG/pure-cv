c2af6132d0730654293f59397af9a2eb8cc6c7b6
Hong <hongwang9851@gmail.com>
2019-08-24 21:52:49 -0400

    change email

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ window.onload = function() {
     document.getElementById("email").innerHTML = '<a href="mailto:' + 'contact' + '@' + 'hongwang' + '.' + 'me"' + 'target="_top">' + 'contact' + '@' + 'hongwang' + '.' + 'me</a>';
 
     var contactform =  document.getElementById('contact-form');
-    contactform.setAttribute('action', '//formspree.io/' + 'contact' + '@' + 'hongwang' + '.' + 'me');
+    contactform.setAttribute('action', '//formspree.io/' + 'hongwang9851' + '@' + 'gmail' + '.' + 'com');
 }
