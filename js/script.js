7831e59b9a0e391bf79ac89c9b1e1b6e4eb25416
Hong <hongwang9851@gmail.com>
2019-08-25 00:10:58 -0400

    Edit js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 window.onload = function() {
-    document.getElementById("email").innerHTML = '<a href="mailto:' + 'contact' + '@' + 'hongwang' + '.' + 'me"' + 'target="_top">' + 'contact' + '@' + 'hongwang' + '.' + 'me</a>';
+    document.getElementById("email").innerHTML = '<a href="mailto:' + 'contact' +
+        '@' + 'hongwang' + '.' + 'me"' + 'target="_top">' + 'contact' + '@' +
+        'hongwang' + '.' + 'me</a>';
 
     var contactform = document.getElementById('contact-form');
-    contactform.setAttribute('action', '//formspree.io/' + 'hongwang9851' + '@' + 'gmail' + '.' + 'com');
+    contactform.setAttribute('action', '//formspree.io/' + 'hongwang9851' +
+        '@' + 'gmail' + '.' + 'com');
 }
