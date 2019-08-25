8615ebaa8967aa47186faede21b6d2a0c1102981
Hong <hongwang9851@gmail.com>
2019-08-24 22:01:06 -0400

    Update contact form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     document.getElementById("email").innerHTML = '<a href="mailto:' + 'contact' + '@' + 'hongwang' + '.' + 'me"' + 'target="_top">' + 'contact' + '@' + 'hongwang' + '.' + 'me</a>';
 
-    var contactform =  document.getElementById('contact-form');
+    var contactform = document.getElementById('contact-form');
     contactform.setAttribute('action', '//formspree.io/' + 'hongwang9851' + '@' + 'gmail' + '.' + 'com');
 }
