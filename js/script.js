481393cafbf04d719ab0300b0848e743cee3b883
Hong <hongwang9851@gmail.com>
2019-08-24 21:37:17 -0400

    Update CSS Update CV Add Google reCAPTCHA v2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-    document.getElementById("email").innerHTML = '<a href="mailto:contact@hongwang.me" target="_top">contact@hongwang.me</a>';
+    document.getElementById("email").innerHTML = '<a href="mailto:' + 'contact' + '@' + 'hongwang' + '.' + 'me"' + 'target="_top">' + 'contact' + '@' + 'hongwang' + '.' + 'me</a>';
+
+    var contactform =  document.getElementById('contact-form');
+    contactform.setAttribute('action', '//formspree.io/' + 'contact' + '@' + 'hongwang' + '.' + 'me');
 }
