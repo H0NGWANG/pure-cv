7994621cee4533a920670e62ed3f104afbe1b8ac
Hong <hongwang9851@gmail.com>
2020-10-07 13:27:46 -0400

    major update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 window.onload = function() {
-    document.getElementById("email").innerHTML = '<a href="mailto:' + 'contact' +
-        '@' + 'hongwang' + '.' + 'me"' + 'target="_top">' + 'contact' + '@' +
-        'hongwang' + '.' + 'me</a>';
-
     var contactform = document.getElementById('contact-form');
-    contactform.setAttribute('action', '//formspree.io/' + 'hongwang9851' +
+    contactform.setAttribute('action', '//formspree.io/' + 'name' +
         '@' + 'gmail' + '.' + 'com');
 }
